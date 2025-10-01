ce36cde88e0bc2230b9503adac141cadcc8fc724
Tihamer <tihamertnt@gmail.com>
2025-10-01 15:12:35 -0500

    hi

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const { readFile } = require('fs').promises;
+const path = require('path');
+const app = express();
+app.get('/', async (request, response) => {
+    response.send(await readFile(path.join(__dirname + '/home.html'), 'utf8'));
+});
+app.listen(process.env.PORT || 3000, (err) => {
+    if (err) {
+        console.log(err);
+    }
+}); // heyyy
