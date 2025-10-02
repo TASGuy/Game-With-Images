80e8a658d08b54b5d49dd31e04d5bfc04a9ec85c
Tihamer <tihamertnt@gmail.com>
2025-10-02 15:03:10 -0500

    hi

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
+// Get Express and ReadFile and Path:
 const express = require('express');
 const { readFile } = require('fs').promises;
 const path = require('path');
+// Get app:
 const app = express();
-app.get('/', async (request, response) => {
-    response.send(await readFile(path.join(__dirname + '/home.html'), 'utf8'));
+
+// Handle subdomains:
+var subdomains = [];
+app.use((request, response, next) => {
+    let parts = request.hostname.split('.');
+    response.send(`<p>${JSON.stringify(parts)}</p>`);
+    next();
 });
+
+// Handle static files:
+app.use('/', express.static(path.join(__dirname + '/public')));
+
+// Handle page not found:
+app.use((request, response) => {
+    response.status(404).send('<h1 style="font-family: monospace, sans-serif;">Status: 404. Page not found.</h1>');
+});
+
+// Start listening on port:
 app.listen(process.env.PORT || 3000, (err) => {
     if (err) {
         console.log(err);
